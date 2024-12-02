d96a25cc359c16f191210a63387c5281bfacb1cf
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-12-02 09:47:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+
+const targetElement = document.getElementById("level");
+
+function findDomLevel(element) {
+  let level = 0;
+  let currentElement = element;
+
+  
+  while (currentElement) {
+    level++;
+    currentElement = currentElement.parentElement;
+  }
+
+  return level;
+}
+
+
+const domLevel = findDomLevel(targetElement);
+
+
+alert(`The level of the element is: ${domLevel}`);
